b525405a0e2f082c23580ad9efb3ff77eff2c86d
Shreyansh Kumar <shreyanshkumar5491@gmail.com>
2026-08-29 12:45:19 +0530

    Add Dockerfile and .dockerignore for multi-stage build setup; enhance backend to serve static files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,31 @@
 import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import "dotenv/config";
+import fs from "fs";
+import path from "path";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import { error } from "console";
 const app = express();
 
 const PORT = process.env.PORT;
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
+
+// if the public directory exists, serve the static files
+// this is for production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
