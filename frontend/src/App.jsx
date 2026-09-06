e6d279763aa039f1ae72479091e332a1566c444e
Shreyansh Kumar <shreyanshkumar5491@gmail.com>
2026-09-06 20:18:57 +0530

    feat: integrate theme and wallpaper context providers, add authentication pages, and update dependencies
    - Added ThemeProvider and WallpaperProvider for managing themes and wallpapers.
    - Created AuthPage and ChatPage components for user authentication and chat functionality.
    - Updated package.json to include new dependencies for theming and styling.
    - Added various wallpapers and sound assets to the public directory.
    - Refactored App component to utilize React Router for navigation between AuthPage and ChatPage.
    - Removed unused CSS styles and integrated Tailwind CSS and HeroUI styles.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,55 @@
-import { useState } from "react";
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import "./App.css";
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage"; //
+import AuthPage from "./pages/AuthPage"; //
+import { useAuth } from "@clerk/react";
+// import PageLoader from "./components/PageLoader";//
+// import { useAuthStore } from "./store/useAuthStore";//
+import { useEffect } from "react";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  // option 1
+  // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
+
+  // // option 2 - better for performance
+  // const clearAuth = useAuthStore((state) => state.clearAuth);
+  // const checkAuth = useAuthStore((state) => state.checkAuth);
+  // const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  // useEffect(() => {
+  //   if (!isLoaded) return;
+
+  //   if (isSignedIn) checkAuth();
+  //   else clearAuth();
+  // }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  // if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
   return (
-    <>
-      <div>My Meesaing app</div>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal"/>
-          <SignUpButton mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />
+            }
+          />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+        <Toaster />
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
